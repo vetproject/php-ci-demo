f18c2266954d1b334dec8f84fb31148d5f5ff5c8
vetproject <vrbvet3@gmail.com>
2025-11-12 15:07:14 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'functions.php';
+
+echo "Welcome to PHP CI/CD Demo<br>";
+echo "2 + 3 = " . add(2, 3);
---
+++ tests/FunctionsTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../functions.php';
+
+class FunctionsTest extends TestCase {
+    public function testAdd() {
+        $this->assertEquals(5, add(2, 3));
+    }
+}
