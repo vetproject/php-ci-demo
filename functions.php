2d3048937300fa5e3583f322acc614d898804ab6
vetproject <vrbvet3@gmail.com>
2025-11-12 15:11:03 +0700

    Add subtract function

--- functions.php
+++ functions.php
@@ -2,3 +2,7 @@
 function add($a, $b) {
     return $a + $b;
 }
+
+function subtract($a, $b) {
+    return $a - $b;
+}
--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -1,10 +1,18 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 
 require_once __DIR__ . '/../functions.php';
 
-class FunctionsTest extends TestCase {
-    public function testAdd() {
+class FunctionsTest extends TestCase
+{
+    public function testAdd()
+    {
         $this->assertEquals(5, add(2, 3));
+    }
+
+    public function testSubtract()
+    {
+        $this->assertEquals(2, subtract(5, 3));
     }
 }
